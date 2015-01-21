95e1f427b53bcb4ae0ef40e9ac6cce3c11f68083
Sam David <sfd1987@gmail.com>
2015-01-21 14:03:01 -0800

    adds bike surface

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@
 // import dependencies
 var Engine = famous.core.Engine;
 var Modifier = famous.core.Modifier;
+var StateModifier = famous.modifiers.StateModifier;
 var Transform = famous.core.Transform;
 var ImageSurface = famous.surfaces.ImageSurface;
+var Surface = famous.core.Surface;
 
 // create the main context
 var mainContext = Engine.createContext();
@@ -15,13 +17,29 @@ var logo = new ImageSurface({
     classes: ['double-sided']
 });
 
-var initialTime = Date.now();
-var centerSpinModifier = new Modifier({
-    origin: [0.5, 0.5],
-    align: [0.5, 0.5],
-    transform : function () {
-        return Transform.rotateY(.002 * (Date.now() - initialTime));
-    }
+var bike = new Surface({
+	size: [400,200],
+	content: 'bike',
+	properties: {
+		backgroundColor: 'red',
+		textAlign: 'center',
+		color: 'white'
+	}
 });
 
-mainContext.add(centerSpinModifier).add(logo);
+var bikeOriginModifier = new StateModifier({
+	origin: [0.5, 0.8],
+  align: [0.5, 0.5]
+	// transform: Transform.translate(100, 10, 0)
+})
+
+// var initialTime = Date.now();
+// var centerSpinModifier = new Modifier({
+//     origin: [0.5, 0.5],
+//     align: [0.5, 0.5],
+//     transform : function () {
+//         return Transform.rotateY(.002 * (Date.now() - initialTime));
+//     }
+// });
+
+mainContext.add(bikeOriginModifier).add(bike);
